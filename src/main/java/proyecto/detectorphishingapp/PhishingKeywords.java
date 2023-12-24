4d04a14e257d26c19f8b529fb8a2b2d242049b18
robjurado <149337753+Roberth4566@users.noreply.github.com>
2023-12-24 16:09:36 -0500

    Actualizar clase PhishingKeywords
    La clase PhishingKeywords,java fue mejorada completando una lista de 30 palabras a las cuales le hemos dado una puntuación del 1 al 3

--- src/main/java/proyecto/detectorphishingapp/PhishingKeywords.java
+++ src/main/java/proyecto/detectorphishingapp/PhishingKeywords.java
@@ -28,7 +28,28 @@ public class PhishingKeywords {
         keywordsMap.put("contraseña", 2);  // Algo probable
         keywordsMap.put("urgente", 3);  // Muy probable
         keywordsMap.put("premio", 2);  // Algo probable
-
+        keywordsMap.put("ganador", 3);  // Muy probable
+        keywordsMap.put("oferta", 2);  // Algo probable
+        keywordsMap.put("exclusiva", 3);  // Muy probable
+        keywordsMap.put("tarjeta", 2);  // Algo probable
+        keywordsMap.put("crédito", 2);  // Algo probable
+        keywordsMap.put("ganaste", 3);  // Muy probable
+        keywordsMap.put("seleccionado", 3);  // Muy probable
+        keywordsMap.put("actualización", 2);  // Algo probable
+        keywordsMap.put("inicio de sesión", 3);  // Muy probable
+        keywordsMap.put("expirado", 2);  // Algo probable
+        keywordsMap.put("confirme", 3);  // Muy probable
+        keywordsMap.put("identidad", 2);  // Algo probable
+        keywordsMap.put("cierre", 3);  // Muy probable
+        keywordsMap.put("felicidades", 2);  // Algo probable
+        keywordsMap.put("secreto", 3);  // Muy probable
+        keywordsMap.put("gratis", 2);  // Algo probable
+        keywordsMap.put("limitado", 3);  // Muy probable
+        keywordsMap.put("oficial", 2);  // Algo probable
+        keywordsMap.put("empresa", 1);  // Poco probable
+        keywordsMap.put("confidencial", 3);  // Muy probable
+        keywordsMap.put("cancelar", 2);  // Algo probable
+        keywordsMap.put("llama ahora", 3);  // Muy probable
 
         return keywordsMap;
     }
